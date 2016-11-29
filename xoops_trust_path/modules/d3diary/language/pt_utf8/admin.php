0bdf0711fb6953013b7385eae5de91d3e8780676
Leco1 <ohsepoa@gmail.com>
2016-11-29 13:26:28 -0200

    Create admin.php

---
+++ xoops_trust_path/modules/d3diary/language/pt_utf8/admin.php
@@ -0,0 +1,30 @@
+<?php
+
+// D3DIARY IMPORT
+define('_MD_D3DIARY_H2_IMPORTFROM','Importar');
+define('_MD_D3DIARY_H3_IMPORTDATABASE','Copiar dados para o módulo d3diario');
+define('_MD_D3DIARY_BTN_DOIMPORT','Executar a importação');
+define('_MD_D3DIARY_LABEL_SELECTMODULE','Selecionar um módulo');
+define('_MD_D3DIARY_CONFIRM_DOIMPORT','Você realmente deseja executar a importação?');
+
+define('_MD_D3DIARY_HELP_IMPORTFROM','Você pode importar somente do módulo d3diary ou minidiário.');
+define('_MD_D3DIARY_IMPORTDONE','A importação está concluida.');
+define('_MD_D3DIARY_ERR_INVALIDMID','Não foi possível executar a importação do módulo que você selecionou.');
+define('_MD_D3DIARY_SQLONIMPORT','A importação falhou.<br />Possívelmente existe diferenças na estrutura das tabelas do banco de dados, entre as tabelas de importação e as tabelas de destino.<br />Por favor, verifique se os dois módulos já foram atualizados para suas versões mais recentes.');
+
+define('_MD_D3DIARY_H3_IMPORTCOM','mover os dados dos comentários do xoops');
+define('_MD_D3DIARY_HELP_COMIMPORT','Importação do comentário movidos dos comentários. <br />Você não pode desfazer depois da execução.');
+define('_MD_D3DIARY_CONFIRM_DOCOMIMPORT','Você realmente deseja mover os comentários?');
+
+define('_MD_D3DIARY_H3_IMPORTNOTIF','Mover os dados de notificação do xoops');
+define('_MD_D3DIARY_HELP_NOTIFIMPORT','Importação de notificação movidas de notificações. <br />Você não pode desfazer depois da execução.');
+define('_MD_D3DIARY_CONFIRM_DONOTIFIMPORT','Você realmente deseja mover as notificações?');
+
+// For D3pipes Options
+define('_MD_D3DIARY_MAXTEXT','Comprimento máximo do texto:');
+define('_MD_D3DIARY_TOPICSPOSTS','Usar bloco:');
+define('_MD_D3DIARY_USEAGGRE','Usar agragação com outros fóruns:');
+define('_MD_D3DIARY_CATLIMIT','ID da categoria');
+define('_MD_D3DIARY_FORUMLIMIT','ID do Fórum');
+
+?>
